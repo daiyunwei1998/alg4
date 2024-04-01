17a5b711edf3d8f44f7038ee30e86f66e8379ff8
daiyunwei1998 <daiyunwei1998@qq.com>
2024-04-01 09:44:07 +0800

    basic structure

---
+++ seam/SeamCarver.java
@@ -0,0 +1,12 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+package PACKAGE_NAME;
+public class SeamCarver {
+    public static void main(String[] args) {
+        
+    }
+}
